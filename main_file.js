ca14d3fb76691036372b840b17f4d22169f84e56
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-06-17 13:54:38 +0600

    added insert and find functionality of database from UI

--- main_file.js
+++ main_file.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({extended: false}))
 // dependencies and other include files and configurations ends here
 
 
+
+
 // homepage
 app.get('/', function(req, res){
     res.send(`<html lang="en">
@@ -131,308 +133,335 @@ app.get('/', function(req, res){
 
 // teachers section starts here
 app.get('/teachers', function(req, res){
-    res.send(`<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="teachers.css">
-        <style>
+    db.collection('vivaSystem').find().sort({"_id": -1}).toArray(function(err, vivaSystem) {
+        
+        res.send(`<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="teachers.css">
+            <style>
+              
+        /* nav starts here */
+        nav {
+            display: flex;
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            padding: 20px 20px 20px 20px;
+            justify-content: space-between;
+            background-color: black;
+            color: white;
+            width: 100%;
+            z-index: 10;
+            
+          }
+          @media(max-width: 700px) {
+            nav{
+            flex-direction: column;
+            }
+          }
           
-    /* nav starts here */
-    nav {
-        display: flex;
-        position: fixed;
-        top: 0px;
-        left: 0px;
-        padding: 20px 20px 20px 20px;
-        justify-content: space-between;
-        background-color: black;
-        color: white;
-        width: 100%;
-        z-index: 10;
+          .nav-link {
+            color: white;
+            text-decoration: none;
+          }
+          nav li {
+            display: inline-block;
+            padding: 20px 50px 10px 20px;
+            border-radius: 13px;
+            }
+          nav li:hover{
+             background-color: rgb(55, 143, 224);
+           
+            
+          }
+           nav li:hover a{
+            color: rgb(0, 2, 128);
+             letter-spacing: 2px;
+          }
+          .logo {
+            max-width: 50px;
+            clip-path: circle(50% at 50% 50%);
+            
+          }
         
-      }
-      @media(max-width: 700px) {
-        nav{
-        flex-direction: column;
-        }
-      }
-      
-      .nav-link {
-        color: white;
-        text-decoration: none;
-      }
-      nav li {
-        display: inline-block;
-        padding: 20px 50px 10px 20px;
-        border-radius: 13px;
-        }
-      nav li:hover{
-         background-color: rgb(55, 143, 224);
-       
+          /* nav finished here */
         
-      }
-       nav li:hover a{
-        color: rgb(0, 2, 128);
-         letter-spacing: 2px;
-      }
-      .logo {
-        max-width: 50px;
-        clip-path: circle(50% at 50% 50%);
-        
-      }
-    
-      /* nav finished here */
-    
-      
-      .subHeader, .submittedlist {
-        margin-top: 140px;
-        left: 0px;
-        margin-left: auto;
-        margin-right: auto;
-        width: 80%;
-        border: 1px solid green;
-        text-align: center;
-        border-radius: 14px;
-        background-color: rgb(0, 22, 95);
-      }
-    
-    
-      input[type=submit], input[type=text], input[type=number]{
-        display: inline-block;
-        font-size: 1em;
-        background: rgb(255, 255, 255);
-        padding: 10px 30px;
-        text-transform: uppercase;
-        text-decoration: none;
-        font-weight: 500;
-        margin-top: 10px;
-        margin-right: 2px;
-        color: #111;
-        letter-spacing: 2px;
-        transition: 0.2s;
-        border-radius: 12px;
-        margin-bottom: 10px;
-        
-      }
-      input:hover{
-        background-color: rgb(24, 209, 255);
-        color: rgb(45, 0, 104);
-        transform: scale(1.1);
-        border: 5px solid white;
-        
-      }
-    /* sub header ends here */
-    
-    /* submitted list begins here */
-    
-    .submittedlist table {
-        /* background: rgb(0, 225, 255); */
-        border: 1px solid whitesmoke;
-        border-radius: 12px;
-        margin: 20px 0 20px 0;
-        margin-left: auto;
-        margin-right: auto;
-        font-size: 20px;
-        color: rgb(255, 255, 255);
-    }
-    
-    .submittedlist td{
-        padding: 2rem 2rem 2rem 2rem;
-        margin: auto;
-    }
-    
-    .submittedlist th {
-        border:1px soild rgb(24, 209, 255);
-    }
-        </style>
-        <title>Teacher</title>
-    </head>
-    <body>
-      <header id="header">
-        <nav id="nav-bar">
-        <li><img id="header-img" src="https://hstu.ac.bd/img/hstu_correct_logo.png" alt="hstu logo" class="logo"></li>
-        <li><a class="nav-link" href="/">Home</a></li>
-        <li><a class="nav-link" href="/teachers">Teachers</a></li>
-        <li><a class="nav-link" href="/students">Students</a></li>
           
-        </nav>
-      </header>
-    
-          <div class="subHeader">
-            <form action="/teachers" method="POST"></form>
-                <input type="number" placeholder="IDNumber" name="idnumber">
-                <input type="text" placeholder="Marks" name="marks">
-                <input type="submit">
-          </div>
-    
-          <div class="submittedList">
-              <table>
-                  
-                  <tr><th>ID Number</th><th>Marks</th></tr> 
-                <tr><td>1802063</td><td>A+</td> <td><input type="submit" value="Edit"></td><td><input type="submit" value="Delete"></td></tr>
-                
-    
-            </table>
-          </div>
-    </body>
-    </html>`)  
-})
-app.post('/teachers', function(req, res) {
-    db.collection('vivaSystem').insertOne({
-        idnumber: req.body.idnumber,
-        marks: req.body.marks,
-        function(){
-            res.redirect('/')
+          .subHeader, .submittedlist {
+            margin-top: 140px;
+            left: 0px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 80%;
+            border: 1px solid green;
+            text-align: center;
+            border-radius: 14px;
+            background-color: rgb(0, 22, 95);
+          }
+        
+        
+          input[type=submit], input[type=text], input[type=number]{
+            display: inline-block;
+            font-size: 1em;
+            background: rgb(255, 255, 255);
+            padding: 10px 30px;
+            text-transform: uppercase;
+            text-decoration: none;
+            font-weight: 500;
+            margin-top: 10px;
+            margin-right: 2px;
+            color: #111;
+            letter-spacing: 2px;
+            transition: 0.2s;
+            border-radius: 12px;
+            margin-bottom: 10px;
+            
+          }
+          input:hover{
+            background-color: rgb(24, 209, 255);
+            color: rgb(45, 0, 104);
+            transform: scale(1.1);
+            border: 5px solid white;
+            
+          }
+        /* sub header ends here */
+        
+        /* submitted list begins here */
+        
+        .submittedlist table {
+            /* background: rgb(0, 225, 255); */
+            border: 1px solid whitesmoke;
+            border-radius: 12px;
+            margin: 20px 0 20px 0;
+            margin-left: auto;
+            margin-right: auto;
+            font-size: 20px;
+            color: rgb(255, 255, 255);
         }
+        
+        .submittedlist td{
+            padding: 2rem 2rem 2rem 2rem;
+            margin: auto;
+        }
+        
+        .submittedlist th {
+            border:1px soild rgb(24, 209, 255);
+        }
+            </style>
+            <title>Teacher</title>
+        </head>
+        <body>
+          <header id="header">
+            <nav id="nav-bar">
+            <li><img id="header-img" src="https://hstu.ac.bd/img/hstu_correct_logo.png" alt="hstu logo" class="logo"></li>
+            <li><a class="nav-link" href="/">Home</a></li>
+            <li><a class="nav-link" href="/teachers">Teachers</a></li>
+            <li><a class="nav-link" href="/students">Students</a></li>
+              
+            </nav>
+          </header>
+        
+              <div class="subHeader">
+                <form id="form" action="/teachers-submit" method="POST">
+                <input name="idnumber" type="number" placeholder="IDNumber" >
+                    <input name="marks" type="number" placeholder="Marks" >
+                    <input type="submit">
+                </form>
+                    
+              </div>
+        
+              <div class="submittedList">
+                  <table>
+                      
+                      <tr><th>ID Number</th><th>Marks</th></tr> 
+
+                      ${vivaSystem.map(function(anyNameAsParameter){
+                          return `
+                          <tr><td>${anyNameAsParameter.idNumber}</td><td>${anyNameAsParameter.Marks}</td> <td><input type="submit" value="Edit"></td><td><input type="submit" value="Delete"></td></tr>`
+                       
+                      }).join('')}
+
+                   
+                    
+        
+                </table>
+              </div>
+        </body>
+        </html>`)  
     })
+})
+app.post('/teachers-submit', function(req, res) {
+
+// time related configuration and function
+var datetime = new Date();
+console.log(datetime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}))
+
+// data structure inside database
+    db.collection('vivaSystem').insertOne({
+        idNumber: req.body.idnumber,
+        Marks: req.body.marks, 
+        time: datetime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})},
+        function(){
+            res.redirect('/teachers')
+        }
+    )
 })
 // teachers section ends here
 
 
 // student section starts here
 app.get('/students', function(req, res){
-    res.send(`<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="teachers.css">
-        <style>
-          
-    /* nav starts here */
-    nav {
-        display: flex;
-        position: fixed;
-        top: 0px;
-        left: 0px;
-        padding: 20px 20px 20px 20px;
-        justify-content: space-between;
-        background-color: black;
-        color: white;
-        width: 100%;
-        z-index: 10;
-        
-      }
-      @media(max-width: 700px) {
-        nav{
-        flex-direction: column;
-        }
-      }
-      
-      .nav-link {
-        color: white;
-        text-decoration: none;
-      }
-      nav li {
-        display: inline-block;
-        padding: 20px 50px 10px 20px;
-        border-radius: 13px;
-        }
-      nav li:hover{
-         background-color: rgb(55, 143, 224);
-       
-        
-      }
-       nav li:hover a{
-        color: rgb(0, 2, 128);
-         letter-spacing: 2px;
-      }
-      .logo {
-        max-width: 50px;
-        clip-path: circle(50% at 50% 50%);
-        
-      }
-    
-      /* nav finished here */
-    
-      
-      .subHeader, .submittedlist {
-        margin-top: 140px;
-        left: 0px;
-        margin-left: auto;
-        margin-right: auto;
-        width: 80%;
-        border: 1px solid green;
-        text-align: center;
-        border-radius: 14px;
-        background-color: rgb(0, 22, 95);
-      }
-    
-    
-      input[type=submit], input[type=text], input[type=number]{
-        display: inline-block;
-        font-size: 1em;
-        background: rgb(255, 255, 255);
-        padding: 10px 30px;
-        text-transform: uppercase;
-        text-decoration: none;
-        font-weight: 500;
-        margin-top: 10px;
-        margin-right: 2px;
-        color: #111;
-        letter-spacing: 2px;
-        transition: 0.2s;
-        border-radius: 12px;
-        margin-bottom: 10px;
-        
-      }
-      input:hover{
-        background-color: rgb(24, 209, 255);
-        color: rgb(45, 0, 104);
-        transform: scale(1.1);
-        border: 5px solid white;
-        
-      }
-    /* sub header ends here */
-    
-    /* submitted list begins here */
-    
-    .submittedlist table {
-        /* background: rgb(0, 225, 255); */
-        border: 1px solid whitesmoke;
-        border-radius: 12px;
-        margin: 20px 0 20px 0;
-        margin-left: auto;
-        margin-right: auto;
-        font-size: 20px;
-        color: rgb(255, 255, 255);
-    }
-    
-    .submittedlist td{
-        padding: 2rem 2rem 2rem 2rem;
-        margin: auto;
-    }
-    
-    .submittedlist th {
-        border:1px soild rgb(24, 209, 255);
-    }
-        </style>
-        <title>Teacher</title>
-    </head>
-    <body>
-    <header id="header">
-        <nav id="nav-bar">
-        <li><img id="header-img" src="https://hstu.ac.bd/img/hstu_correct_logo.png" alt="hstu logo" class="logo"></li>
-        <li><a class="nav-link" href="/">Home</a></li>
-        <li><a class="nav-link" href="/teachers">Teachers</a></li>
-        <li><a class="nav-link" href="/students">Students</a></li>
-          
-        </nav>
-      </header>
-
-     
-
-      <div class="submittedList">
-          <table>
+    db.collection('vivaSystem').find().sort({"_id": -1}).toArray(function(err, vivaSystem){
+        res.send(`<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="teachers.css">
+            <style>
               
-              <tr><th>ID Number</th><th>TimeStamp</th></tr> 
-            <tr><td>1802063</td><td>4.50 PM</td> <td><input type="submit" value="Edit"></td><td><input type="submit" value="Delete"></td></tr>
+        /* nav starts here */
+        nav {
+            display: flex;
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            padding: 20px 20px 20px 20px;
+            justify-content: space-between;
+            background-color: black;
+            color: white;
+            width: 100%;
+            z-index: 10;
             
+          }
+          @media(max-width: 700px) {
+            nav{
+            flex-direction: column;
+            }
+          }
+          
+          .nav-link {
+            color: white;
+            text-decoration: none;
+          }
+          nav li {
+            display: inline-block;
+            padding: 20px 50px 10px 20px;
+            border-radius: 13px;
+            }
+          nav li:hover{
+             background-color: rgb(55, 143, 224);
+           
+            
+          }
+           nav li:hover a{
+            color: rgb(0, 2, 128);
+             letter-spacing: 2px;
+          }
+          .logo {
+            max-width: 50px;
+            clip-path: circle(50% at 50% 50%);
+            
+          }
+        
+          /* nav finished here */
+        
+          
+          .subHeader, .submittedlist {
+            margin-top: 140px;
+            left: 0px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 80%;
+            border: 1px solid green;
+            text-align: center;
+            border-radius: 14px;
+            background-color: rgb(0, 22, 95);
+          }
+        
+        
+          input[type=submit], input[type=text], input[type=number]{
+            display: inline-block;
+            font-size: 1em;
+            background: rgb(255, 255, 255);
+            padding: 10px 30px;
+            text-transform: uppercase;
+            text-decoration: none;
+            font-weight: 500;
+            margin-top: 10px;
+            margin-right: 2px;
+            color: #111;
+            letter-spacing: 2px;
+            transition: 0.2s;
+            border-radius: 12px;
+            margin-bottom: 10px;
+            
+          }
+          input:hover{
+            background-color: rgb(24, 209, 255);
+            color: rgb(45, 0, 104);
+            transform: scale(1.1);
+            border: 5px solid white;
+            
+          }
+        /* sub header ends here */
+        
+        /* submitted list begins here */
+        
+        .submittedlist table {
+            /* background: rgb(0, 225, 255); */
+            border: 1px solid whitesmoke;
+            border-radius: 12px;
+            margin: 20px 0 20px 0;
+            margin-left: auto;
+            margin-right: auto;
+            font-size: 20px;
+            color: rgb(255, 255, 255);
+        }
+        
+        .submittedlist td{
+            padding: 2rem 2rem 2rem 2rem;
+            margin: auto;
+        }
+        
+        .submittedlist th {
+            border:1px soild rgb(24, 209, 255);
+        }
+            </style>
+            <title>Teacher</title>
+        </head>
+        <body>
+        <header id="header">
+            <nav id="nav-bar">
+            <li><img id="header-img" src="https://hstu.ac.bd/img/hstu_correct_logo.png" alt="hstu logo" class="logo"></li>
+            <li><a class="nav-link" href="/">Home</a></li>
+            <li><a class="nav-link" href="/teachers">Teachers</a></li>
+            <li><a class="nav-link" href="/students">Students</a></li>
+              
+            </nav>
+          </header>
+    
+         
+    
+          <div class="submittedList">
+              <table>
+                  
+                  <tr><th>ID Number</th><th>TimeStamp</th></tr> 
 
-        </table>
-      </div>
-</body>
-    </html>`)  
+                ${vivaSystem.map(function(anyName){
+                    return `<tr><td>${anyName.idNumber}</td><td>${anyName.time}</td></tr>`
+                    
+                })}
+
+                
+    
+            </table>
+          </div>
+    </body>
+        </html>`)  
+        
+    })
 })
