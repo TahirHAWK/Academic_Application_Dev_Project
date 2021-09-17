70ad140ce3faf802119d12ed8590a0c31737e7b9
TahirHSTU <tahirtamin20@gmail.com>
2021-09-17 22:33:56 +0600

    some modification to run it

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 let {express, app, nodemailer, mongodb, connectionString, transporter} = require('./export_files/dependencies')
 
+let port = process.env.PORT
+if(port == null || port == ""){
+  port = 3000
+}
 
 let databaseConnect = mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client){
     
   db = client.db() 
-  app.listen(3000)
+  app.listen(port)
 }) 
 
 module.exports = {databaseConnect}
